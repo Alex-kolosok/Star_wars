b159568bbb2b6f56702ad4dece6cf348f11fd363
Master <alex.mikh.2004@gmail.com>
2021-01-17 20:45:39 +0300

    Import libraries2

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -2,3 +2,9 @@ from pygame import *
 import sys
 from os.path import abspath, dirname
 from random import choice
+
+
+BASE_PATH = abspath(dirname(__file__))
+FONT_PATH = BASE_PATH + '/fonts/'
+IMAGE_PATH = BASE_PATH + '/images/'
+SOUND_PATH = BASE_PATH + '/sounds/'
